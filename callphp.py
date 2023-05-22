090feb947fa9c52e906dd11724ba54b9de4fc22a
Kamthorn Tan <dr.kamthorn.tan@gmail.com>
2023-05-22 08:38:33 +0700

    first commit

---
+++ callphp.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+# embed streamlit docs in a streamlit app
+#components.iframe("https://www.baidu.com")
+
+st.markdown('<a href="http://sigj.atwebpages.com/check/index.php" target="_self"></a>', unsafe_allow_html=True)
